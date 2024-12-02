5af6f6a55bed802407f2db0c90c6c5a271b27756
Manas Ranjan Adha <manas865873@gmail.com>
2024-12-02 21:04:40 +0530

    add about and notfound route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 
 import { Route, Routes } from "react-router";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
 
 function Home() {
   return (
@@ -15,6 +17,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
